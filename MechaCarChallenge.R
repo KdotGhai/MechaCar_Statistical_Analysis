0620a9dea7372b65e29651259e4aa01263938963
Kartikye <Legionnaires_51@outlook.com>
2022-04-08 17:04:35 -0400

    applied revisions

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,26 +1,35 @@
 library(dplyr)
 
-### DELIVERABLE 1 ###
+                                        ######** DELIVERABLE 1 **#####
 # Import the data
 mecha_car_mpg <- read.csv(file='MechaCar_mpg.csv',check.names = F, stringsAsFactors = F)
 
 # Perform linear regression that predicts the mpg of MechaCar prototypes
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mecha_car_mpg)
+
+# Use summary() function to determine p-value,r^2 value for linear regression model(lm)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mecha_car_mpg))
+
 # p-value: 5.35e-11
-# r-squared: 0.6825
+# Adjusted r-squared: 0.6825  (PREFERRED SINCE ITS MORE ACCURATE FOR EACH INDEPENDENT VARIABLE ADDED)
+# Multiple r^2:0.7149
 
 
-### DELIVERABLE 2 ###
+                                        ######** DELIVERABLE 2 **######
+
 # Import the data
 sus_coil <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
 # Create a summary data frame
 total_summary <- sus_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
 # Create a summary data frame grouped by Manufacturing Lot
 lot_summary <- sus_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI),Variance = var(PSI), SD = sd(PSI))
 
 
-### DELIVERABLE 3 ###
+
+                                        ######** DELIVERABLE 3 **######
+
 # Determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch
 t.test(sus_coil$PSI, mu = 1500)
 
